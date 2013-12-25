8a85a78af4b771261137133aaf68c257e5f1be6a
chenbingkun55 <chenbingkun55@163.com>
2013-12-25 21:02:40 +0800

    初始化项目

---
+++ Gomyhome/Conf/config.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+	// 开启应用分组.
+	'APP_GROUP_LIST' => 'Index,Admin,Myt',
+	'DEFAULT_GROUP' => 'Index',
+
+	// 数据库连接参数.
+	'DB_HOST' => 'localhost',
+	'DB_USER' => '',
+	'DB_PWD' => '',
+	'DB_NAME' => '',
+	'DB_PREFIX' => '',
+	
+	// 点语法默认解析
+	'TMPL_VAR_IDENTIFY' => 'array',
+);
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?PHP
+
+	error_reporting(E_ERROR | E_WARNING | E_PARSE);
+	define('APP_NAME','Gomyhome');
+	define('APP_PATH','./Gomyhome/');
+	define('APP_DEBUG',TRUE);
+
+	define('THINK_PATH','./ThinkPHP/');
+	require(THINK_PATH.'/ThinkPHP.php');
+
+?>
